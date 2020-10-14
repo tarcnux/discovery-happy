fe0346ac9d54e55704ab7462bbb57353319c4676
Tarcisio Nunes <github@tarcnux.com.br>
2020-10-14 00:33:46 -0300

    Alterar ícone e criar texto popup

--- public/scripts/page-orfanatos.js
+++ public/scripts/page-orfanatos.js
@@ -8,7 +8,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+//Criar ícone
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170,2]
+})
+
+//Criar popup overley
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Orfanato Alpha <a href="orfanato.html?id=1" \
+class="escolher-orfanato"><img src="./public/images/arrow-white.svg"></a>')
+
 //Marcação no Mapa
-L.marker([-27.1156927,-48.9123907]).addTo(map)
-    .bindPopup('SENAI de Brusque/SC')
-    .openPopup();
+L.marker([-27.1156927,-48.9123907], {icon}).addTo(map)
+    .bindPopup(popup);
