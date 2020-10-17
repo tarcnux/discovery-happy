e7ae5882e935704df667eca52ee4eeaf0e8b5e17
Tarcisio Nunes <github@tarcnux.com.br>
2020-10-17 11:38:37 -0300

    Criar Database SQLite Testar inserir e consultar tudo

---
+++ src/database/test.js
@@ -0,0 +1,32 @@
+const Database = require('./db')
+
+Database.then( async db => {
+    //inserir dados na tabela
+    await db.run(`
+        INSERT into orfanatos (
+            lat,
+            lng,
+            name,
+            about,
+            whatsapp,
+            images,
+            instructions,
+            visite_time,
+            open_on_weekends
+        ) VALUES (
+            '-27.1156927',
+            '-48.9123907',
+            'Orfanato Alpha',
+            'Presta assitência à crianças Alpha',
+            '(47) 99999-8888',
+            'https://images.unsplash.com/photo-1600711724779-c039d7410ceb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+            'Venha com amor, se sinta à vontade e traga muita esperança para distribuir.',
+            'Horário de visitas das 10h às 15h',
+            '1'
+        )
+    `)
+    //Consultar dados na tabela
+
+    const orfanatosSelecionados = await db.all('SELECT * FROM orfanatos')
+    console.log(orfanatosSelecionados);
+})
